3f0786a916e1e1134f64a156922422946318da02
SKD <sarthakd762@gmail.com>
2021-06-01 00:23:09 +0530

    l

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
 import "./App.css";
 import Navbar from "./Navbar/navbar.js";
-import Login from "./Login/login";
+import Login from "./Login/signup.js";
 
 function App() {
   return (
     <div>
       <Login />
+      {/* <Navbar /> */}
     </div>
   );
 }
--- src/Login/signup.js
+++ src/Login/signup.js
@@ -1,59 +1,26 @@
-import React from 'react'
-import styles from './signup.css';
-import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
-import GoogleLogin from 'react-google-login';
+import React from "react";
+import "./signup.css";
 
-const responseFacebook = (response) => {
-  console.log(response);
-}
- const responseGoogle=(response)=>{
-    console.log(response);
-    // console.log(response.profileObj);
-}
-
-const signup = () =>{
-    return(
+const signup = () => {
+  return (
+    <div>
+      <form action="" class="container">
+        <h1>Signup</h1>
+        Username
         <div>
-     <form action="" class="container">
-         <h1 >Signup</h1>
-         Username
-         <div><input type="text" size="50"/></div>
-         Email address
-         <div><input type="text" /></div>
-         Password
-         <div><input type="password" /></div>
-         <button>Sign Up</button>
-
-         <h6><span>or</span></h6>
-         <div>
-         <GoogleLogin className="google"
-         clientId="576795438351-pmcgd8452klkcdtf4k72024hnchkdjdl.apps.googleusercontent.com"
-         buttonText="Google"
-         //  onSuccess={this.reponseGoogle}
-         //  onFailure={this.reponseGoogle}
-         cookiePolicy={'single_host_origin'}
-         
-         />
-          {/* <FacebookLogin className="fb"
-    appId="915284599319290"
-    autoLoad={true}
-    fields="name,email,picture"
-    //onClick={componentClicked}
-    callback={responseFacebook} 
-    /> */}
-    <FacebookLogin 
-  appId="915284599319290"
-  autoLoad
-  callback={responseFacebook}
-  render={renderProps => (
-    <button class="fb" onClick={renderProps.onClick}><span>Facebook</span></button>
-  )}
-/>
-         </div>
-
-
-     </form>
+          <input type="text" size="50" />
+        </div>
+        Email address
+        <div>
+          <input type="text" />
+        </div>
+        Password
+        <div>
+          <input type="password" />
+        </div>
+        <button>Sign Up</button>
+      </form>
     </div>
-    )
-}
-export default signup
+  );
+};
+export default signup;
