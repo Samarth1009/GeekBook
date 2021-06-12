565a3af5f6c892239a2a2bfe2c4b5c7c2bc293f0
SKD <sarthakd762@gmail.com>
2021-06-12 18:12:36 +0530

    char ui added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import "./App.css";
 import Navbar from "./Navbar/navbar.js";
 // import Login from "./Login/login.js";
 // import Profile from './Profile/profile.js'
-import Home from './Home/Home'
+import Home from "./Home/Home";
+import Chat from "./Home/Chats/Chat";
+import { useState } from "react";
 
 function App() {
+  const [chat_show, setChat_show] = useState(null);
   return (
-    <div className='app'>
+    <div className="app">
       {/* <Login /> */}
       <Navbar />
       <Home />
+      <Chat
+        show={chat_show}
+        handleClose={() => {
+          setChat_show(null);
+        }}
+      />
+      <div
+        className="chat_open_button"
+        style={chat_show && { display: "none" }}
+        onClick={() => {
+          setChat_show(true);
+        }}
+      >
+        Chat a Geek
+      </div>
     </div>
   );
 }
