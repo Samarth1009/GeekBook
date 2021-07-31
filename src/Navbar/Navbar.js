b10b880aa1c343d670fb7531d9ef3379f01d63f0
Shashwat Kushwaha <vasuk8324@gmail.com>
2021-07-31 23:45:03 +0530

    dropdown added in navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,8 +1,23 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
+import {useState} from "react";
 import styles from "./Navbar.module.css";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import { Button,Menu,MenuItem } from "@material-ui/core";
 
-const navbar = () => {
+
+
+const Navbar = () => {
+  
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
   return (
     <nav className={styles.navbar}>
       <h3 className={styles.title}>GeekBook</h3>
@@ -10,11 +25,29 @@ const navbar = () => {
         type="text"
         name="search"
         id={styles.search}
-        placeholder="Search"
-      ></input>
-      <Avatar className={styles.myavatar} />
+        placeholder="Search"> 
+        </input>
+        
+        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        <Avatar className={styles.myavatar} />
+        </Button>
+        <Menu
+        className="nav_dropdown"
+         id="simple-menu"
+         anchorEl={anchorEl}
+         keepMounted
+         open={Boolean(anchorEl)}
+         onClose={handleClose}
+         >
+         
+         <MenuItem className="nav_dropdown_item">Profile</MenuItem>
+         <MenuItem className="nav_dropdown_item">Signup</MenuItem>
+         <MenuItem className="nav_dropdown_item">Signout</MenuItem>
+         </Menu>
+
+   
     </nav>
   );
 };
 
-export default navbar;
+export default Navbar;
