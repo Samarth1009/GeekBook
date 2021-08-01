fac0f9ce600967e8a7c3c3b00af2aaeff91a2f58
SKD <sarthakd762@gmail.com>
2021-08-01 16:53:26 +0530

    navbar modeuls to css

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,51 +1,41 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
-import {useState} from "react";
-import styles from "./Navbar.module.css";
+import { useState } from "react";
+import "./navbar.css";
 import DropdownButton from "react-bootstrap/DropdownButton";
-import { Button,Menu,MenuItem } from "@material-ui/core";
-
-
+import { Button, Menu, MenuItem } from "@material-ui/core";
 
 const Navbar = () => {
-  
-    const [anchorEl, setAnchorEl] = React.useState(null);
-  
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-  return (
-    <nav className={styles.navbar}>
-      <h3 className={styles.title}>GeekBook</h3>
-      <input
-        type="text"
-        name="search"
-        id={styles.search}
-        placeholder="Search"> 
-        </input>
-        
-        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-        <Avatar className={styles.myavatar} />
-        </Button>
-        <Menu
-        className="nav_dropdown"
-         id="simple-menu"
-         anchorEl={anchorEl}
-         keepMounted
-         open={Boolean(anchorEl)}
-         onClose={handleClose}
-         >
-         
-         <MenuItem className="nav_dropdown_item">Profile</MenuItem>
-         <MenuItem className="nav_dropdown_item">Signup</MenuItem>
-         <MenuItem className="nav_dropdown_item">Signout</MenuItem>
-         </Menu>
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-   
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <nav className="navbar">
+      <h3 className="title">GeekBook</h3>
+      <input type="text" name="search" id="search" placeholder="Search"></input>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem className="nav_dropdown_item">Profile</MenuItem>
+        <MenuItem className="nav_dropdown_item">Signup</MenuItem>
+        <MenuItem className="nav_dropdown_item">Signout</MenuItem>
+      </Menu>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <Avatar className="myavatar" />
+      </Button>
     </nav>
   );
 };
